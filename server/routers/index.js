4206f102faab250c8dea068b051277d84aa34aed
皮宇轩 <piyuxuan@lightwit.cn>
2020-05-27 18:13:27 +0800

    接口

--- server/routers/index.js
+++ server/routers/index.js
@@ -1,5 +1,5 @@
 const KoaRouter = require('koa-router');
-
+const md5 = require('md5');
 const Models = require('../models')
 
 
@@ -8,23 +8,14 @@ const router = new KoaRouter();
 
 
 router.get('/api/comments', async ctx => {
-  
-    // const users  = await Models.users.findAll()
-    // const contents  = await Models.contents.findAll()
-    //  const comments  = await Models.comments.findAll()
-    //  const likes  = await Models.likes.findAll()
-    // const data = {
-    //     users,
-    //     contents,
-    //     comments,
-    //     likes
-    // }
-    
-
+  console.log(ctx.query)
+     const page = ctx.query.page || 1;
+    const prepage = Number(ctx.query.prepage) || 2 ;
+    const offset = ((page -1) * prepage);
      
     const res  = await Models.contents.findAndCountAll({
-        limit:3,
-        offset:0,
+        limit:prepage,
+        offset,
         include:{
             model:Models.users
         }
@@ -39,17 +30,122 @@ router.get('/api/comments', async ctx => {
             username:el.user.username,
             updatetime:el.updatetime,
             like_count:el.like_count,
-            comment_count:el.comment_count
+            comment_count:el.comment_count,
+            create_time:el.createdAt
             
         }
     })
 
       ctx.body =   {
           code:0,
+          prepage,
           count:res.count,
-          data
+          data,
+          msg:''
       }
 });
 
+router.post('/api/register', async ctx => {
+    console.log(ctx.request.body)
+   const username = ctx.request.body.username;
+   const password = ctx.request.body.password;
+   const repassword = ctx.request.body.repassword;
+   if(username == ''){
+     return  ctx.body = {
+           code:1,
+           data:'',
+           msg:'用户名不能为空'
+       }
+   }
+   if(password == ''){
+    return  ctx.body = {
+          code:1,
+          data:'',
+          msg:'密码不能为空'
+      }
+  }
+  if(password != repassword){
+    return  ctx.body = {
+          code:1,
+          data:'',
+          msg:'两次密码不一致'
+      }
+  }
+  let user = await Models.users.findOne({
+      where:{
+        username
+      }
+   
+  })
+  if(user != null){
+     return  ctx.body = {
+          code:1,
+          data:'',
+          msg:'用户名已重复'
+      }
+  }
+ let newPerson = await Models.users.build({
+     username,
+     password:md5(password)
+ }).save()
 
+ ctx.body = {
+     code:0,
+     data:{
+         id:newPerson.get('id'),
+         username:newPerson.get('username')
+     },
+     msg:'注册成功'
+ }
+})
+
+router.post('/api/login', async ctx => {
+    const username = ctx.request.body.username;
+    const password = ctx.request.body.password ;
+    console.log(password)
+    const getUser = await Models.users.findOne({
+        where:{
+            username
+        }
+    })
+    if(getUser == null){
+        return ctx.body ={
+            code:1,
+            data:'',
+            msg:'没有该用户'
+        }
+    }
+
+    if(username == ''){
+        return ctx.body ={
+            code:1,
+            data:'',
+            msg:'用户名不能为空'
+        }
+    }
+    if(password == ''){
+        return ctx.body ={
+            code:1,
+            data:'',
+            msg:'密码不能为空'
+        }
+    }
+    if(md5(password) != getUser.get('password')){
+        return ctx.body = {
+            code:2,
+            data:'',
+            msg:'用户名或密码不正确'
+        }
+    }else{
+        ctx.body = {
+            code:0,
+            data:{
+                username:getUser.get('username'),
+                id: getUser.get('id')
+            }
+           
+        }
+      
+    }
+})
 module.exports = router;
